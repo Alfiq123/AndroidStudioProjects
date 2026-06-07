84ab71a6aa956159fd34a0fe0ad93afcf0e96285
Alfiq123 <112615436+Alfiq123@users.noreply.github.com>
2026-06-07 09:40:59 +0700

    🥇 Version 1.0.1 SC

---
+++ Fast_HTML_Alpha/app/src/main/java/com/fasthtml/fasthtmlalpha/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.fasthtml.fasthtmlalpha
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fasthtml.fasthtmlalpha.ui.theme.Fast_HTML_AlphaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Fast_HTML_AlphaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Fast_HTML_AlphaTheme {
+        Greeting("Android")
+    }
+}
